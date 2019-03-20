2da8214c20717f18f40457bcf382ff855b310a89
akdsco <arkadiusz@ostrowski.co>
2019-03-20 10:49:25 +0000

    smooth scroll working

---
+++ js/customJavaScript.js
@@ -0,0 +1,55 @@
+// "Main function to operate actions on website"
+$(function(){
+	
+	// Make collapsed menu close on click (Mobile Devices)
+	$('#my-navbar a').on('click', function() {
+            $('.navbar-collapse').collapse('hide');
+        });
+
+	// w3c solution to smooth scrolling
+
+  // Add scrollspy to <body>
+  $('body').scrollspy({target: "#my-navbar"});   
+
+  // Add smooth scrolling on all links inside the navbar
+  $("#my-navbar a").on('click', function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    }
+  });
+});
+
+// Custom JavaScript to validate form input
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
+
